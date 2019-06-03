b6a2b5d99ec084192713ea826c148b664f7825da
sugarcube <auztin410@gmail.com>
2019-06-03 19:36:25 -0400

    First part of the the auto carousel done.

--- function.js
+++ function.js
@@ -1,7 +1,15 @@
 function fadeIn(c) {
-$(c).attr("#fadeIn");
+$(c).attr("id", "fadeIn");
+setTimeout(() => {
+    fadeOut(c)
+}, 3500);
 };
 
 function fadeOut(c) {
-$(c).attr("#fadeOut");
+$(c).attr("id", "fadeOut");
+setTimeout(() => {
+    fadeIn(c)
+}, 3500);
 };
+
+$(document).ready(fadeOut(".firstImage"));
