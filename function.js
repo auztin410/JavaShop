a1004196435114ce7aa913047abe2ef2ca0fd3ca
sugarcube <auztin410@gmail.com>
2019-06-03 19:54:54 -0400

    Building the auto carousel to allow for any number of images to be used

--- function.js
+++ function.js
@@ -12,4 +12,14 @@ setTimeout(() => {
 }, 3500);
 };
 
+function nextInArray(arr) {
+console.log(arr);
+arr.push(arr.shift());
+console.log(arr);
+};
+
 $(document).ready(fadeOut(".firstImage"));
+
+var arr = [1,2,3,4];
+
+$(document).ready(nextInArray(arr));
